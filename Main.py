cffea43f5458b537bf98c1435ac6ad5b9f3c38e4
rexrex600 <jdavies0703@hotmail.co.uk>
2016-04-24 20:23:05 +0100

    Patched mute to use global ID to prevent circumvention

--- Main.py
+++ Main.py
@@ -1,16 +1,22 @@
 import discord
 import asyncio
-import pickle
 
+# Initialising the discord client
 test = discord.Client()
+
+# Opening the MuteList log
 file = open('MuteList.txt', 'r')
 
+# Processing the MuteList file and loading the mute list from the file
 mute_list = []
 mute_list = file.read().split(',')
 
-commands = ['join', 'shutdown', 'leave', 'smashcapitalism', 'mute', 'unmute']
-chats = ['rocketleague', 'menofwar', 'minecraft', 'paradox','politicalmachine','ksp', 'gta']
+# Initialising the list of commands and valid chats for command-based joining
+commands = ['join', 'shutdown', 'leave', 'smashcapitalism', 'mute', 'unmute', 'lableme']
+chats = ['rocketleague', 'menofwar', 'minecraft', 'paradox', 'politicalmachine', 'ksp', 'gta', 'civ']
 
+
+# Logging client login details
 @test.event
 async def on_ready():
     print('Logged in as')
@@ -19,41 +25,54 @@ async def on_ready():
     print('------')
 
 
+# Commands function
 @test.event
 async def on_message(message):
+    # Invoking global variables
     global mute_list
     global file
     contents = message.content
+    # Checking for the command character
     if contents[0] == '!':
-        print("command: ",  contents, "author: ", message.author)
+        # Logging the command and the author for moderation purposes
+        print("command: ", contents, "author: ", message.author)
+        # Processing the command for parsing
         contents = contents[1::]
         contents = contents.split(' ')
         if contents[0] in commands:
+            # Join channel command - adds role
             if contents[0] == 'join' and contents[1] in chats:
                 role = message.server.roles
                 for i in role:
                     if i.name == contents[1].lower():
                         await test.add_roles(message.author, i)
+            # Leave channel command - removes role
             elif contents[0] == 'leave' and contents[1] in chats:
                 role = message.server.roles
                 for i in role:
                     if i.name == contents[1].lower():
                         await test.remove_roles(message.author, i)
+            # Generic tesxt response command
             elif contents[0] == 'smashcapitalism':
                 await test.send_message(message.channel, ':bomb: SMASH CAPITALISM :bomb:')
+            # Admin Command - shuts down the bot (atm restricted to rexrex600 because I have local access and am able to restart the bot
             elif contents[0] == 'shutdown' and message.author.name == 'rexrex600':
                 file.close()
                 await test.delete_message(message)
                 await test.close()
+            # The general case of someone other than me trying to shut down the bot - 'Fuck off'
             elif contents[0] == 'shutdown' and message.author.name != 'rexrex600':
                 await test.send_message(destination=message.channel, content='Fuck off', tts=True)
+            # Mute command
             elif contents[0] == 'mute' and message.author.permissions_in(message.channel).kick_members is True:
+                member_list = test.get_all_members()
+                target_user = [i for i in member_list if i.name in contents[1]][0]
                 file = open('MuteList.txt', 'r')
                 mute_list = file.read().split(',')
                 mute_list = [i for i in mute_list if i != '']
-                if contents[1] not in mute_list:
+                if target_user.id not in mute_list:
                     file.close
-                    mute_list.append(contents[1])
+                    mute_list.append(target_user.id)
                     file = open('MuteList.txt', 'w')
                     dump_str = ''
                     for i in mute_list:
@@ -62,14 +81,16 @@ async def on_message(message):
                 else:
                     await test.send_message(message.author, 'user is already muted')
                 file.close()
-
+            # Unmute command
             elif contents[0] == 'unmute' and message.author.permissions_in(message.channel).kick_members is True:
+                member_list = test.get_all_members()
+                target_user = [i for i in member_list if i.name in contents[1]][0]
                 file = open('MuteList.txt', 'r')
                 mute_list = file.read().split(',')
                 mute_list = [i for i in mute_list if i != '']
-                if contents[1] in mute_list:
+                if target_user.id in mute_list:
                     file.close()
-                    mute_list.remove(contents[1])
+                    mute_list.remove(target_user.id)
                     file = open('MuteList.txt', 'w')
                     dump_str = ''
                     for i in mute_list:
@@ -78,10 +99,12 @@ async def on_message(message):
                 else:
                     await test.send_message(message.author, 'user is not muted')
                 file.close()
+            # elif contents[0] == 'lableme':
+            #     await test.add_roles(test.create_role(server = message.server, name = contents[1], permissions = discord.Permissions.none(), colour = (contents[2], contents[3], contents[4]), hosit = True, ))
         else:
             await test.send_message(destination=message.channel, content='Invalid Command')
         await test.delete_message(message)
-    if ('#' + str(message.author).split('#')[1]) in mute_list:
+    if message.author.id in mute_list:
         await test.delete_message(message)
 
 
